99f7676ce44755425590c47bf486fba3e46b9641
Diego Czajka <67860687+diegoHDCz@users.noreply.github.com>
2022-02-23 23:16:46 -0300

    deploy aws

--- serverless.ts
+++ serverless.ts
@@ -11,6 +11,7 @@ const serverlessConfiguration: AWS = {
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    region: 'us-east-1',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
@@ -19,6 +20,14 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
+  
+    iamRoleStatements: [
+      {
+        Effect:"Allow",
+        Action:["dynamodb:*"],
+        Resource: ["*"]
+      }
+    ]
   },
   // import the function via paths
   functions: {
@@ -37,7 +46,7 @@ const serverlessConfiguration: AWS = {
   },
   package: { individually: true },
   custom: {
-    esbuild: {  
+    esbuild: {
       bundle: true,
       minify: false,
       sourcemap: true,
@@ -77,7 +86,7 @@ const serverlessConfiguration: AWS = {
               AttributeName: 'id',
               KeyType: 'HASH',
             },
-          ], 
+          ],
         },
       },
     },
